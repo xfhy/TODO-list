307b8afed5e5154811050b50d3ffbdad712fe8e1
xfhy666 <xfhy666@gmail.com>
2018-08-15 21:49:59 +0800

    完成开发

--- app/src/main/java/com/xfhy/todo/activity/SplashActivity.kt
+++ app/src/main/java/com/xfhy/todo/activity/SplashActivity.kt
@@ -1,9 +1,15 @@
 package com.xfhy.todo.activity
 
+import android.animation.Animator
 import android.os.Bundle
-import com.tbruyelle.rxpermissions2.RxPermissions
+import android.view.Window
+import android.view.WindowManager
 import com.xfhy.library.basekit.activity.BaseActivity
+import com.xfhy.library.utils.SPUtils
 import com.xfhy.todo.R
+import com.xfhy.todo.common.Constant
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.startActivity
 
 /**
  * 2018年8月14日17:33:40
@@ -14,9 +20,27 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)// 隐藏标题
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
-        val rxPermissions = RxPermissions(this)
+        mSplashAnimationView.addAnimatorListener(object:Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
 
+            override fun onAnimationEnd(animation: Animator?) {
+                if(SPUtils.getValue(Constant.IS_LOGIN,false)){
+                    startActivity<MainActivity>()
+                } else {
+                    startActivity<LoginActivity>()
+                }
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
     }
 }
